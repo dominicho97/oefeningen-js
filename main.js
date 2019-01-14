2b25d96e9c13dd919a00f84445bfcef3ccec245e
dominicho97 <dominic.ho97@gmail.com>
2019-01-14 09:32:01 +0100

    added console.log

--- main.js
+++ main.js
@@ -2,3 +2,15 @@ const beroep = " web developer";
 const land = "belgië";
 const vrouw = "toekomstige vrouw";
 const kinderen = 3;
+
+console.log(
+  "U wordt" +
+    beroep +
+    " en woont in " +
+    land +
+    ", en bent getrouwd met " +
+    vrouw +
+    " met " +
+    kinderen +
+    " kinderen."
+);
