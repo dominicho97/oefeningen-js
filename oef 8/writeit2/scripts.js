8d72329d64d1c085dea7d32f415ed25310ac7cf9
dominicho97 <dominic.ho97@gmail.com>
2019-01-15 16:40:47 +0100

    added const movies, let movieUpper and document.write, not finished

---
+++ oef 8/writeit2/scripts.js
@@ -0,0 +1,7 @@
+const movies = [
+  "Hotel Transylvania 3: Summer Vacation",
+  "Don't Worry, He Won't Get Far on Foot",
+  "Hollywood aan de Schelde"
+];
+
+document.write("<ul>" + movies + "</ul>");
