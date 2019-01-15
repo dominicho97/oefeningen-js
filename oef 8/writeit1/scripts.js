cf4569ce6823d117ce524d2f4c0b4c301479a47f
dominicho97 <dominic.ho97@gmail.com>
2019-01-15 16:33:12 +0100

    corrected excercise

--- oef 8/writeit1/scripts.js
+++ oef 8/writeit1/scripts.js
@@ -1,7 +1,7 @@
 const movies = [
-  "Hotel Transylvania 3: Summer Vacation",
+  ("Hotel Transylvania 3: Summer Vacation",
   "Don't Worry, He Won't Get Far on Foot",
-  "Hollywood aan de Schelde"
+  "Hollywood aan de Schelde").toUpperCase()
 ];
 
 document.write("<ul>" + movies + "</ul>");
