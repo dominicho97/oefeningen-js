18c55434cd6884846ca0ec12bc34516fcbd7eb21
dominicho97 <dominic.ho97@gmail.com>
2019-01-16 12:06:55 +0100

    added tolocalUpperCase() in array

--- oef 8/writeit1/scripts.js
+++ oef 8/writeit1/scripts.js
@@ -1,7 +1,9 @@
 const movies = [
-  ("Hotel Transylvania 3: Summer Vacation",
+  "Hotel Transylvania 3: Summer Vacation",
   "Don't Worry, He Won't Get Far on Foot",
-  "Hollywood aan de Schelde").toUpperCase()
+  "Hollywood aan de Schelde"
 ];
 
-document.write("<ul>" + movies + "</ul>");
+let moviesUpper = movies.toUpperCase();
+
+document.write("<ul>" + moviesUpper + "</ul>");
