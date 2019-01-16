1189501666eb9e6c5db118bb454af51219f35fc3
dominicho97 <dominic.ho97@gmail.com>
2019-01-16 09:25:25 +0100

    added curly brackets and object name:, and age:

---
+++ oef 8/writeit3/scripts.js
@@ -0,0 +1,14 @@
+const bestActors = [
+  { name: "Gary Oldman", age: 60 },
+  { name: "Casey Affleck", age: 43 },
+  { name: "Leonardo DiCaprio", age: 44 },
+  { name: "Eddie Redmayne", age: 37 },
+  { name: "Matthew McConaughey ", age: 49 },
+  { name: "Daniel Day-Lewis", age: 61 },
+  { name: "Jean Dujardin ", age: 46 },
+  { name: "Colin Firth ", age: 58 },
+  { name: "Jeff Bridges ", age: 69 },
+  { name: "Sean Penn ", age: 58 }
+];
+
+document.write("<ul>" + movies + "</ul>");
