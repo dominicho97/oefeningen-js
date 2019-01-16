9996a855e9882488b848996e0f76df236946522d
dominicho97 <dominic.ho97@gmail.com>
2019-01-16 15:02:16 +0100

    added and updated files

---
+++ oef 8/writeit5/scripts.js
@@ -0,0 +1,41 @@
+const fees = [
+  {
+    price: 10.75,
+    title: "Normaal tarief"
+  },
+  {
+    price: 9.75,
+    title: "Kortingstarief"
+  },
+  {
+    price: 8.05,
+    title: "Kinepolis Student Card"
+  }
+];
+
+const extras = [
+  {
+    price: 2.0,
+    title: "Supplement 3D"
+  },
+  {
+    price: 2.0,
+    title: "Supplement Escape"
+  },
+  {
+    price: 3.0,
+    title: "Supplement HFR 3D"
+  },
+  {
+    price: 0.75,
+    title: "Supplement Film Lange Speelduur (>/=2u15)"
+  },
+  {
+    price: 2.5,
+    title: "Supplement Cosy Zone"
+  },
+  {
+    price: 1.0,
+    title: "Supplement Atmos"
+  }
+];
